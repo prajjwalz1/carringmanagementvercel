662531d7943c6f8199ba8eb8b98756625e1d4d76
prajjwal <acharyaprajjwol152@gmail.com>
2023-08-27 00:08:55 +0545

    first commit

--- management/settings.py
+++ management/settings.py
@@ -140,8 +140,3 @@ MEDIA_URL="/media/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-import os
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
-application = get_wsgi_application()
-app = application
