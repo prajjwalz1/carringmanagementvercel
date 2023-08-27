30b5cb6ec3a04d776a03b99103c7e39d8a782091
prajjwal <acharyaprajjwol152@gmail.com>
2023-08-27 10:31:14 +0545

    new database added

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,6 @@
+
  echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
+ python -m pip install psycopg2-binary
+ python -m pip install -r requirements.txt
+ python manage.py collectstatic --noinput --clear
  echo "BUILD END"
-
-
- # Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
