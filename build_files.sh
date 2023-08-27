d45b5fb910be425d29b1cfe9c63edbe64b616f59
prajjwal <acharyaprajjwol152@gmail.com>
2023-08-27 10:50:16 +0545

    new database added

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,13 @@
-
  echo "BUILD START"
- python -m pip install psycopg2-binary
- python -m pip install -r requirements.txt
- python manage.py collectstatic --noinput --clear
+ # python3.9 -m pip install -r requirements.txt
+ python3.9 -m venv venv
+    source venv/bin/activate
+ pip install -r requirements.txt
+ # python3.9 manage.py collectstatic
+ # ls
  echo "BUILD END"
+# source venv/bin/activate
+
+ # Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic
