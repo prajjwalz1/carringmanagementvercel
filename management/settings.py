e1968eb4eb0e500865fce3d4b1e92aee3ee72575
prajjwal <acharyaprajjwol152@gmail.com>
2023-08-27 10:09:44 +0545

    new database added

--- management/settings.py
+++ management/settings.py
@@ -141,8 +141,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# Configures the staticfiles directory to serve
+# static files from /static/ on our deployment
+STATIC_ROOT = os.path.join(
+    BASE_DIR, 'staticfiles', 'static')
 
 
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
