5dd7b616f92171025ae9d3fc0c0bd0d3fb9b1dad
prajjwal <acharyaprajjwol152@gmail.com>
2023-08-27 09:55:52 +0545

    new database added

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,8 @@
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
+
+
+ # Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
