3e3f2182121861d6877a52b3fb77399b6e0b2513
prajjwal <acharyaprajjwol152@gmail.com>
2023-08-27 10:11:39 +0545

    new database added

--- management/settings.py
+++ management/settings.py
@@ -146,6 +146,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # static files from /static/ on our deployment
 STATIC_ROOT = os.path.join(
     BASE_DIR, 'staticfiles', 'static')
+STATIC_URL = '/static/'
 
 
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
